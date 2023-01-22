487703535bc9e6931e9034e924eb1e17e0f363c9
miathy <mainguyen15695@gmail.com>
2023-01-22 11:21:06 +1100

    module changes

---
+++ property.py
@@ -0,0 +1,7 @@
+class Property:
+    def __init__(self, name, price,colour):
+        self.name = name
+        self.price = price
+        self.colour = colour
+        self.owner = None
+        self.rent  = 1
